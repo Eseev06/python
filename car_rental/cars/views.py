547cb38ff72bdc2f714309b7138009ba25363509
Баха <yeseev06@mail.ru>
2025-04-18 18:33:40 +0600

    week 4

--- car_rental/cars/views.py
+++ car_rental/cars/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Car
 
+# Список всех автомобилей
 def car_list(request):
     cars = Car.objects.all()
-    return render(request, 'car_list.html', {'cars': cars})
+    return JsonResponse({'cars': list(cars.values())})
 
+# Детали конкретного автомобиля
 def car_detail(request, pk):
-    car =  (Car, pk=pk)
-    return render(request, 'car_detail.html', {'car': car})
-
+    car = get_object_or_404(Car, pk=pk)
+    return JsonResponse({'car': {'name': car.name, 'brand': car.brand, 'year': car.year, 'price_per_day': car.price_per_day}})
