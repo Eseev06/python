76fc0f7ab43a4d9a3e5de2c137dbb988fc881003
yeraa <erasyl_amangeldi@list.ru>
2025-04-25 09:40:38 +0500

    search

--- anytime123/settings.py
+++ anytime123/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
